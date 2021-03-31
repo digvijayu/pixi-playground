4f2adecf9c915a29912f7097e4caed42a4cf7d3d
Digvijay Upadhyay <d.upadhyay@planfwd.de>
2021-03-31 18:02:05 +0200

    motion

--- stories/Spirits/5-motion.js
+++ stories/Spirits/5-motion.js
@@ -2,7 +2,7 @@ import * as PIXI from "pixi.js";
 
 import images from "../../assets/mario.png";
 
-export const blendmode = () => {
+export const motion = () => {
   let app = new PIXI.Application({
     width: 256,
     height: 256,
@@ -22,11 +22,16 @@ export const blendmode = () => {
     sprites.mario.pivot.x = sprites.mario.width / 2;
     sprites.mario.pivot.y = sprites.mario.height / 2;
     app.stage.addChild(sprites.mario);
+  });
 
-    sprites.mario.tint = Math.random() * 0xffffff;
+  app.ticker.add(() => {
+    if (sprites.mario) {
+      if (sprites.mario.x > window.innerWidth) sprites.mario.x = 0;
+      sprites.mario.x += 1;
+    }
   });
 
   return app.view;
 };
 
-blendmode.storyName = "3-blendmode";
+motion.storyName = "5-motion";
--- stories/Spirits/index.stories.js
+++ stories/Spirits/index.stories.js
@@ -10,3 +10,4 @@ export { spiritDisplay } from "./1-display";
 export { animation } from "./2-animation";
 export { blendmode } from "./3-blendmode";
 export { handleClick } from "./4-handleClick";
+export { motion } from "./5-motion";
